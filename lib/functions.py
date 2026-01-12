4a55d0ed4c7ae2c1cfbbba5f84234463a2ea908c
Brian-melita <brianmelitah@gmail.com>
2026-01-12 23:36:59 +0300

    final

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sunny")
+
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+number = add(1,2)
+print(number)
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+halve(3)
